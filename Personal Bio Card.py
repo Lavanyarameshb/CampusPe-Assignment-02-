770d6c3d8a0e1e2abeef2b67ca4ad6b339a71a59
Lavanyarameshb <lavanya2004r@gmail.com>
2026-02-23 13:57:01 +0530

    Saving Q-01

---
+++ Personal Bio Card.py
@@ -0,0 +1,19 @@
+# Question 1: Personal Bio Card
+
+# Storing student information in variables
+student_name = "Deepu Gowda R"
+student_age = 20
+student_course = "Python Programming"
+student_college = "ABC University"
+student_email = "deepu@example.com"
+
+# Printing formatted bio card
+print("_______________________________________________")
+print("|          STUDENT BIO CARD                    |")
+print("|______________________________________________|")
+print("| Name    : " + student_name +"                |")                
+print("| Age     : " + str(student_age) + " years"+"  |")
+print("| Course  : " + student_course+"               |") 
+print("| College : " + student_college+"              |")
+print("| Email   : " + student_email+"                |")
+print("|______________________________________________|")
