3fca366dc90a011aa2ff8b38af5819dc9b4be370
Lavanyarameshb <lavanya2004r@gmail.com>
2026-02-23 14:26:49 +0530

    Saving Q-01

--- Personal Bio Card.py
+++ Personal Bio Card.py
@@ -1,8 +1,8 @@
 # Question 1: Personal Bio Card
 
 # Storing student information in variables
-student_name = "Deepu Gowda R"
-student_age = 20
+student_name = "Lavanya R"
+student_age = 21
 student_course = "Python Programming"
 student_college = "ABC University"
 student_email = "deepu@example.com"
